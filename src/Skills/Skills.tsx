c66cd20a4915f442ca53b86574dd9f2d9b9f5136
Andriy <refwol131@mail.ru>
2020-08-13 18:33:59 +0300

    initial commit

---
+++ src/Skills/Skills.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import style from './Skills.module.css';
+import styleContainer from "../Common/styles/Container.module.css";
+
+function Skills() {
+    return (
+        <div className={style.skillsBlock}>
+            <div className={styleContainer.container}>
+
+            </div>
+        </div>
+    );
+}
+
+export default Skills;
