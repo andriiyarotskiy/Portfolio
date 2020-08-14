e3e9d56dc8db21f2a104a6d33f582909f4da30b7
Andriy <refwol131@mail.ru>
2020-08-14 17:49:44 +0300

    first step

--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import style from './Skills.module.css';
 import styleContainer from "../Common/styles/Container.module.css";
+import Skill from "./skill/Skill";
 
 function Skills() {
     return (
         <div className={style.skillsBlock}>
-            <div className={styleContainer.container}>
-
+            <div className={`${styleContainer.container} ${style.skillsContainer}`}>
+                <h2 className={style.title}>Skills</h2>
+                <div className={style.skills}>
+                    <Skill title={'js'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, nostrum?'}/>
+                    <Skill title={'html'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, nostrum?'}/>
+                    <Skill title={'react'}
+                           description={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, nostrum?'}/>
+                </div>
             </div>
         </div>
     );
