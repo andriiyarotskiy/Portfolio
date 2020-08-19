0cb60c2e89adb807dbe7cff90470c26806c98151
Andriy <refwol131@mail.ru>
2020-08-19 20:29:23 +0300

    Updates

---
+++ precache-manifest.ed4c3c99f24f957323c7f85a0571eca7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee468a2ce6745288f550807f998d19f",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "265d574c2a8ad81af0d2",
+    "url": "/Portfolio/static/css/main.7dfaf8ce.chunk.css"
+  },
+  {
+    "revision": "ad64307f662a084e65bd",
+    "url": "/Portfolio/static/js/2.7c14ca1f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Portfolio/static/js/2.7c14ca1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "265d574c2a8ad81af0d2",
+    "url": "/Portfolio/static/js/main.dab9c3b1.chunk.js"
+  },
+  {
+    "revision": "c66aa5eafc120060b1dc",
+    "url": "/Portfolio/static/js/runtime-main.efb1fe8d.js"
+  }
+]);
